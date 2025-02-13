365c324932d5a0a07b7401870bb2f57d53042a71
Rubén Ramírez <rruben084@gmail.com>
2025-02-13 20:01:12 +0000

    Proyecto Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-import "bootstrap";
-import "./style.css";
+let intervalId = null; // Variable para almacenar el temporizador
 
+window.onload = function () {
+    generateCard();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    document.getElementById("generator-button").onclick = function () {
+        generateCard();
+    };
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    document.getElementById("start-timer").onclick = startTimer;
+    document.getElementById("stop-timer").onclick = stopTimer;
+}
+
+// Función para generar cartas aleatorias
+function generateCard() {
+    let figures = ["♥", "♦", "♣", "♠"];
+    let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"];
+
+    let figure = figures[Math.floor(Math.random() * figures.length)];
+    let number = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.querySelector("#palosuperior").innerHTML = figure;
+    document.querySelector("#valordelacarta").innerHTML = number;
+    document.querySelector("#paloinferior").innerHTML = figure;
+
+    document.querySelector(".carta").style.color = (figure === "♥" || figure === "♦") ? "red" : "black";
+
+}
+
+function startTimer() {
+    if (!intervalId) {
+        generateCard();
+        intervalId = setInterval(generateCard, 10000);
+    }
+}
+
+function stopTimer() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
